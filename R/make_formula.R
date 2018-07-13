dd3246d4f5bbdbf5cca460305a5f1c79a75d25d0
Devin Caughey <devin.caughey@gmail.com>
2018-07-13 09:37:58 -0400

    fixed make_formula

--- R/make_formula.R
+++ R/make_formula.R
@@ -1,4 +1,8 @@
-make_formula <- function (xvars, yvar = NULL) {
-    xform <- paste(xvars, collapse = " + ")
-    as.formula(paste(yvar, xform, sep = " ~ "))    
+make_formula <- function (var_list) {
+    form_char_vec <- vector("character", length(var_list))
+    for (l in seq_along(var_list)) {
+        form_char_vec[l] <- paste(var_list[[l]], collapse = " + ")
+    }
+    form_char <- do.call(paste, list(form_char_vec, collapse = " ~ "))
+    as.formula(form_char)    
 }
