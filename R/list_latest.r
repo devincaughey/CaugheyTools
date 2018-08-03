04954e2a27033c64292baa65061b6245f7e70c08
Devin Caughey <devin.caughey@gmail.com>
2018-08-03 12:02:01 -0400

    list_latest

---
+++ R/list_latest.r
@@ -0,0 +1,6 @@
+list_latest <- function (str = ".*", dir = ".", ext = ".*") {
+    all_files <- list.files(dir)
+    matching_files <- all_files[grepl(str, all_files) & grepl(ext, all_files)]
+    dates <- sub(".*([0-9]{6}).*", "\\1", matching_files)
+    return(matching_files[which.max(dates)])
+}
