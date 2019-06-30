6a41c58d76c06dec641b3b21644bd359c4bb8a35
Devin Caughey <devin.caughey@gmail.com>
2019-06-30 15:39:55 -0400

    added interval_to_range

---
+++ R/interval_to_range.r
@@ -0,0 +1,12 @@
+interval_to_range <- function (x) {
+    xx <- stringr::str_split(x, ",")
+    xx1 <- sapply(xx, function (yy) yy[1])
+    xx2 <- sapply(xx, function (yy) yy[2])
+    min <- ifelse(stringr::str_detect(xx1, "\\["),
+                  as.integer(stringr::str_sub(xx1, 2, -1)),
+                  as.integer(stringr::str_sub(xx1, 2, -1)) + 1L)
+    max <- ifelse(stringr::str_detect(xx2, "\\]"),
+                  as.integer(stringr::str_sub(xx2, , -2)),
+                  as.integer(stringr::str_sub(xx2, 1, -2)) - 1L)
+    paste0(min, "-", max)
+}
