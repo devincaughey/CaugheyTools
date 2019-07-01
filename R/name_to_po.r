d596df6871259a18a44bf135e1111759dcc3c806
Devin Caughey <devin.caughey@gmail.com>
2019-06-30 21:58:20 -0400

    corrected name_to_po

--- R/name_to_po.r
+++ R/name_to_po.r
@@ -1,4 +1,4 @@
-po_to_name <- function (name, dta = CaugheyTools::states) {
+name_to_po <- function (name, dta = CaugheyTools::states) {
     name <- as.character(name)
     po_abbrevs <- dta$POAbrv[match(name, dta$Name)]
     return(po_abbrevs)
