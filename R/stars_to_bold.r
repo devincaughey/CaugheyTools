dfb99b501ef0f07b5fdbbc41f29439a9d502db02
Devin Caughey <devin.caughey@gmail.com>
2019-07-05 09:50:16 -0400

    added stars_to_bold

---
+++ R/stars_to_bold.r
@@ -0,0 +1,6 @@
+stars_to_bold <- function (text) {
+    out <- gsub("([[:digit:]]+)\\.([[:digit:]]+)\\^\\{\\*+\\}",
+                "\\\\textbf{\\1}.\\\\textbf{\\2}", text)
+    cat(out, sep = "\n")
+    invisible(out)
+}
